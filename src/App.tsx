a09811614c8729b03c83d5e0520bf240d6523c48
Shubham More <shubham.more@joshsoftware.com>
2023-01-31 17:54:25 +0530

    Added App,Index and CSS file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ToastContainer } from "react-toastify";
+
+import RouteComponent from "./Route";
+
+import "./App.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouteComponent />
+      <ToastContainer />
     </div>
   );
 }
 
-export default App;
+export default React.memo(App);
